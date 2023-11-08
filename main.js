a17dfa67cb653b3cd490ca126188bffaf1a2e28f
acabrie <102576895+acabrie@users.noreply.github.com>
2023-11-08 13:51:48 +0000

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function toggleMenu(){
+    const navToggle = document.querySelector('.mobile-nav');
+    const navMenu = document.querySelector('.primary-nav');
+
+    navToggle.classList.toggle('open');
+    navMenu.classList.toggle('open');
+}
+
